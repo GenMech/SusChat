39994a0ce77bc895c820fad6f88ed4f201968896
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-08-24 23:21:06 +0530

    chore: remove gemini code

--- components/main/SearchPageContainer.tsx
+++ components/main/SearchPageContainer.tsx
@@ -1,11 +1,6 @@
 "use client";
 
 import React from "react";
-const {
-  GoogleGenerativeAI,
-  HarmCategory,
-  HarmBlockThreshold,
-} = require("@google/generative-ai");
 
 import { useState, useEffect, KeyboardEvent, ChangeEvent } from "react";
 
@@ -25,63 +20,6 @@ function SearchPageContainer() {
   const [error, setError] = useState<string | null>(null);
   const [showChat, setShowChat] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  // const apiKey = "AIzaSyB-28-4XA34UY8hIhtuT6FFyA108WiiJWU";
-  // const MODEL_NAME = "gemini-1.5-pro";
-  // const genAI = new GoogleGenerativeAI(apiKey);
-
-  // const generationConfig = {
-  //   temperature: 0.9,
-  //   topP: 1,
-  //   topK: 1,
-  //   maxOutputTokens: 1000,
-  //   responseMimeType: "text/plain",
-  // };
-
-  // const safetySettings = [
-  //   {
-  //     category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-  //     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  //   },
-  //   {
-  //     category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-  //     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  //   },
-  //   {
-  //     category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-  //     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  //   },
-  //   {
-  //     category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-  //     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  //   },
-  // ];
-
-  // useEffect(() => {
-  //   const initChat = async () => {
-  //     try {
-  //       const newChat = await genAI
-  //         .getGenerativeModel({ model: MODEL_NAME })
-  //         .startChat({
-  //           generationConfig,
-  //           safetySettings,
-  //           history: messages.map((msg) => ({
-  //             role: msg.role,
-  //             parts: [
-  //               {
-  //                 text: msg.text,
-  //               },
-  //             ],
-  //             // text: msg.text,
-  //           })),
-  //         });
-  //       setChat(newChat);
-  //     } catch (error) {
-  //       setError("Failed to initialize chat. Please try again.");
-  //     }
-  //   };
-  //   initChat();
-  // }, []);
 
   const handleSendMessage = async () => {
     try {
