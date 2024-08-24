40f77748e99e11ad70bc9b911cd6120da735792b
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-08-25 01:32:27 +0530

    fix

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,29 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
-import pRetry from "p-retry";
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-    const data = await pRetry(
-      () =>
-        fetch(
-          "https://xkdcd1ic4k.execute-api.ap-southeast-2.amazonaws.com/dev/sq",
-          {
-            method: "POST",
-            headers: {
-              "x-api-key": process.env.X_QUERY_SEARCH_API_KEY,
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(body),
-          }
-        ).then((response) => {
-          if (!response.ok)
-            throw new Error(`API responded with status ${response.status}`);
-          return response.json();
-        }),
-      { retries: 1 }
+    const response = await fetch(
+      "https://xkdcd1ic4k.execute-api.ap-southeast-2.amazonaws.com/dev/sq",
+      {
+        method: "POST",
+        headers: {
+          "x-api-key": process.env.X_QUERY_SEARCH_API_KEY,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }
     );
 
+    const data = await response.json();
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json(
