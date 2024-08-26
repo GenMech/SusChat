e90085b3c05f065a37cfe7da0a46c575e5d19a57
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-08-26 17:09:51 +0530

    fix: modal update, error bug fix

--- components/main/RecentlyAddedModal.tsx
+++ components/main/RecentlyAddedModal.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import { useEffect, useRef } from "react";
 import Image from "next/image";
 import { FaExternalLinkAlt } from "react-icons/fa";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 interface RecentlyAddedItem {
   image: string;
@@ -42,50 +50,60 @@ const RecentlyAddedModal: React.FC<RecentlyAddedModalProps> = ({
       onClick={onClose}
     >
       <div
-        className="flex flex-col bg-[#31313140] backdrop-blur-[6.8px] p-6 rounded-lg max-w-[60%] h-[70vh] max-h-[90vh] transform translate-x-[5%] sm:translate-x-[10%] lg:translate-x-[110px]"
+        className="relative flex flex-col bg-[#31313140] backdrop-blur-[6.8px] p-6 rounded-lg lg:w-[50%] max-w-[60%] lg:h-[50vh] h-[58vh] max-h-[90vh] transform translate-x-[110px]"
         onClick={(e) => e.stopPropagation()}
         ref={modalRef}
       >
-        <div className="flex-grow overflow-y-auto">
-          <h2 className="text-2xl font-bold mb-4">Recently Added</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 pl-2 mb-1">
-            {recentlyAddedItems.map((item, index) => (
-              <div
-                key={index}
-                className="bg-white bg-opacity-80 border-[1px] hover:shadow-lg rounded-md border-slate-500 shadow shadow-slate-500 p-4 flex flex-col items-center cursor-pointer"
-              >
-                <div className="flex flex-row justify-start items-start w-full gap-3">
-                  <Image
-                    src={item.image}
-                    alt={item.name}
-                    width={40}
-                    height={40}
-                    className="object-cover rounded-md mb-4"
-                  />
-                  <h3 className=" text-black text-lg font-semibold my-1">
-                    {item.name}
-                  </h3>
-                </div>
-                <p className="text-sm text-black mb-4 line-clamp-5">
-                  {item.description}
-                </p>
-                <a
-                  href={item.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex flex-row items-center gap-1 text-blue-500 hover:underline"
-                >
-                  <p className="text-base">Link</p>
-                  <FaExternalLinkAlt className="pb-[2px]" />
-                </a>
-              </div>
-            ))}
+        <h2 className="text-2xl font-bold mb-4">Recently Added</h2>
+        <div className="flex-grow flex items-center justify-center">
+          <div className="relative flex items-center w-full justify-center">
+            <Carousel className="w-full max-w-[88%] relative">
+              <CarouselContent>
+                {recentlyAddedItems.map((item, index) => (
+                  <CarouselItem key={index}>
+                    <div className="p-1">
+                      <Card className="bg-white bg-opacity-80 h-64 overflow-y-auto">
+                        <CardContent className="flex flex-col items-center pt-4">
+                          <div className="flex flex-row justify-start items-center w-full gap-3 pb-3">
+                            <Image
+                              src={item.image}
+                              alt={item.name}
+                              width={40}
+                              height={40}
+                              className="object-cover rounded-md w-16 h-16"
+                            />
+                            <span className="py-3">
+                              <h3 className="text-black text-lg font-semibold my-1">
+                                {item.name}
+                              </h3>
+                            </span>
+                          </div>
+                          <p className="text-base text-black mb-4 line-clamp-3">
+                            {item.description}
+                          </p>
+                          <a
+                            href={item.link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="lg:absolute lg:bottom-4 text-blue-500 hover:underline"
+                          >
+                            <p className="text-lg">Link</p>
+                          </a>
+                        </CardContent>
+                      </Card>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious className="absolute -left-12 top-1/2 transform -translate-y-1/2 z-10" />
+              <CarouselNext className="absolute -right-12 top-1/2 transform -translate-y-1/2 z-10" />
+            </Carousel>
           </div>
         </div>
-        <div className="mt-4 pt-4">
+        <div className="lg:mt-4 pt-2">
           <button
             onClick={onClose}
-            className="lg:absolute lg:bottom-4 lg:right-4 self-end mt-4 text-red-500 hover:text-red-700"
+            className="lg:absolute lg:bottom-4 lg:right-4 flex items-center justify-center text-red-500 hover:text-red-700"
           >
             Close
           </button>
